551bda6e9e2793001b0ba24958e56ae7394cf0c6
Kyle Mathews <mathews.kyle@gmail.com>
2021-07-12 14:14:59 -0700

    Remove process-top

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-const top = require('process-top')()
-
-setInterval(function () {
-  // Prints out a string containing stats about your Node.js process.
-  console.log(top.toString())
-}, 1000)
 
 module.exports = {
   siteMetadata: {
