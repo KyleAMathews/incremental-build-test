08ec2898883114a19a6e231c82111cb625934969
Kyle Mathews <mathews.kyle@gmail.com>
2021-07-12 13:59:58 -0700

    use canary version of gatsby-plugin-gatsby-cloud to test streaming writes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
+const top = require('process-top')()
+
+setInterval(function () {
+  // Prints out a string containing stats about your Node.js process.
+  console.log(top.toString())
+}, 1000)
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Benchmark Create Pages`,
     description: `The createPages benchmark`,
     author: `@gatsbyjs`,
   },
-  // plugins: [`gatsby-plugin-benchmark-reporting`],
+  plugins: [`gatsby-plugin-gatsby-cloud`],
 }
