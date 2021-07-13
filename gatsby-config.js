0e54a6da73dc11c46d5f65295ecac99dc8c5fdac
Kyle Mathews <mathews.kyle@gmail.com>
2021-07-13 12:32:10 -0700

    Add back memory logger

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const top = require('process-top')()
+
+setInterval(function () {
+  // Prints out a string containing stats about your Node.js process.
+  console.log(top.toString())
+}, 1000)
 
 module.exports = {
   siteMetadata: {
